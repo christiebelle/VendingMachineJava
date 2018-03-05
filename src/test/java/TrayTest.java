f59ef96312bbb9a024c71c9378ecedb11ebe553e
Christiebelle <cslester@outlook.com>
2018-03-05 13:31:25 +0000

    Tray test, setup and first test written

---
+++ src/test/java/TrayTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TrayTest {
+
+    private Tray tray;
+
+    @Before
+    public void setUp() throws Exception {
+        tray = new Tray(0.50, 10);
+    }
+
+    @Test
+    public void testTrayPrice() {
+        assertEquals(0.50, tray.getPrice());
+    }
+
+    //check trays have a price
+    //check tray stock
+    //check we can add an item to a tray
+    //check we can remove an item from a tray
+    //check we cannot add items beyond a tray's capacity
+}
