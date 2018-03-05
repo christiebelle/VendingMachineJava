a959ac53fef737bc8cd685b37ea00462e8acb1ec
Christiebelle <cslester@outlook.com>
2018-03-05 21:03:32 +0000

    Extension: Coin Enum Added and Machine Float test class created

---
+++ src/main/java/Coins.java
@@ -0,0 +1,7 @@
+public enum Coins {
+    TWOPOUND(2.00),
+    ONEPOUND(1.00),
+    FIFTY(0.50),
+    TWENTY(0.20),
+    TEN(0.10),
+}
---
+++ src/test/java/MachineFloatTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+
+import java.util.HashMap;
+
+public class MachineFloatTest {
+
+    private MachineFloat machineFloat;
+
+
+    @Before
+    public void setUp() throws Exception {
+        HashMap<Coins, Integer> bank = new HashMap<>();
+
+
+    }
+}
