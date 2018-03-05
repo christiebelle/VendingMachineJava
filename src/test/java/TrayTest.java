1e12696ab4f5b2c03397c1cb63d228ebb8543f8a
Christiebelle <cslester@outlook.com>
2018-03-05 13:56:17 +0000

    Test and method for ejecting an item from the tray

--- src/test/java/TrayTest.java
+++ src/test/java/TrayTest.java
@@ -12,16 +12,19 @@ public class TrayTest {
         tray = new Tray(0.50, 10);
     }
 
+    //check trays have a price
     @Test
     public void testTrayPrice() {
         assertEquals(0.50, tray.getPrice(), 0.01);
     }
 
+    //check tray stock
     @Test
     public void testStockLevelZero() {
         assertEquals(0, tray.getStockLevel());
     }
 
+    //check we can add an item to a tray
     @Test
     public void testCanAddItem() {
         Drink drink = new Drink("Ice Tea", 500);
@@ -29,9 +32,14 @@ public class TrayTest {
         assertEquals(1, tray.getStockLevel());
     }
 
-    //check trays have a price
-    //check tray stock
-    //check we can add an item to a tray
     //check we can remove an item from a tray
+    @Test
+    public void testTrayEjectsItem() {
+        Drink drink1 = new Drink("Ice Tea", 500);
+        tray.addItem(drink1);
+        Drink ejectedDrink = (Drink) tray.ejectProduct();
+        assert(ejectedDrink instanceof Drink);
+    }
+
     //check we cannot add items beyond a tray's capacity
 }
