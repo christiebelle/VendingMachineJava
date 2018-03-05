955200e2098915940a4cd83911f323ac5590a9ea
Christiebelle <cslester@outlook.com>
2018-03-05 22:20:56 +0000

    Example float initialised and possible test methods laid out in comments

--- src/test/java/MachineFloatTest.java
+++ src/test/java/MachineFloatTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 
 public class MachineFloatTest {
 
@@ -11,6 +14,34 @@ public class MachineFloatTest {
     public void setUp() throws Exception {
         HashMap<Coins, Integer> bank = new HashMap<>();
 
+        bank.put(Coins.TWOPOUND, 5);
+        bank.put(Coins.ONEPOUND, 5);
+        bank.put(Coins.FIFTY, 5);
+        bank.put(Coins.TWENTY, 5);
+        bank.put(Coins.TEN, 5);
 
+    //Can this be done in a for loop?
     }
+
+    //Test Float has money
+
+//    @Test
+//    public void testFloatHasMoney() {
+//
+//    }
+
+    //Test can return Float total
+
+    @Test
+    public void testFloatTotal() {
+        assertEquals(19.0, bank.getFloatTotal());
+    }
+
+
+    //Test can check amount of individual coins
+
+    //Test can add to float
+
+    //Test can remove from float
+
 }
