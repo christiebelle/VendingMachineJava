d100cfe49300d77078a4ad6d17005050920af18f
Christiebelle <cslester@outlook.com>
2018-03-05 12:08:25 +0000

    Sweet class created and tests passing

--- src/test/java/SweetTest.java
+++ src/test/java/SweetTest.java
@@ -3,13 +3,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class SweetTest extends Product {
+public class SweetTest {
 
     Sweet sweet;
 
     @Before
     public void setUp() throws Exception {
-        sweet = new Sweet("Refreshers", 50);
+        sweet = new Sweet("Refreshers", 10);
     }
 
     @Test
